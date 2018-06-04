c64f4371a93f6d2ee596ce1938366fc6564acf17
Luke Mwila <outlier.developer@gmail.com>
2018-06-04 06:34:50 +0200

    Initial push

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Posts from './Posts/Posts'
+import { Route } from 'react-router-dom'
+
+import './Blog.css';
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className='Blog'>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/*<Route 
+                    path='/'
+                    exact
+                    render={() => <h1>Home</h1>}
+                />*/}
+                <Route 
+                path='/' 
+                exact 
+                component={Posts} 
+                />
+            </div>
+        );
+    }
+}
+
+export default Blog;
